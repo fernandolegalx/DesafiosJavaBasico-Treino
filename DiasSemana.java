f7b2501293d95acc6e59c85110d8b86cb0f51d17
Nano <116908088+fernandolegalx@users.noreply.github.com>
2022-11-09 14:21:04 -0300

    Update DiasSemana.java

--- DiasSemana.java
+++ DiasSemana.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class desafiodiasemana{
+public class DiaSemana{
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		System.out.print("Digite o nome do dia: ");
